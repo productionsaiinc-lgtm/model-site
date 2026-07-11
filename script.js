aabf2d28d8cb089a9c01de8cd770b98f38b842fb
nate <271378545+productionsaiinc-lgtm@users.noreply.github.com>
2026-07-10 20:16:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = "https://nova-studio-api.onrender.com";
+const API_URL = "https://model-site-alpha.vercel.app";
 document.addEventListener('DOMContentLoaded', () => {
     const ageGate = document.getElementById('age-gate');
     const btnYes = document.querySelector('.btn-yes');
